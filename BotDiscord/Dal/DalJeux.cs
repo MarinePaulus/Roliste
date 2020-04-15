8c49c5b7db5ec296031b5e87be65ae5e652f00d7
Marine <Marine@192.168.0.15>
2020-04-15 14:59:50 +0200

    Dal Jeux & Personne

---
+++ BotDiscord/Dal/DalJeux.cs
@@ -0,0 +1,63 @@
+﻿using BotDiscord.BdD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BotDiscord.Dal
+{
+    class DalJeux : IDisposable
+    {
+        private ModelRoliste bdd;
+
+        public DalJeux()
+        {
+            bdd = new ModelRoliste();
+        }
+          
+        public bool AddJeu(string nom, Personne mj)
+        {
+            try
+            {
+                Jeux jeux = bdd.Jeux.FirstOrDefault(jeu => jeu.nomjeux == nom && jeu.idmj == mj.idperso);
+                if (jeux == null) {
+                    bdd.Jeux.Add(new Jeux { nomjeux=nom, idmj=mj.idperso});
+                    bdd.SaveChanges();
+                    return true;
+                } else { Console.WriteLine("Le Jeu existe déjà, impossible de la rajouter."); return false; }
+            } catch (Exception e) { Console.WriteLine(e.Message); return false; }
+        }
+        public bool UpJeu(string nom, Personne mj, string newnom, Personne newmj)
+        {
+            try
+            {
+                Jeux jeux = bdd.Jeux.FirstOrDefault(jeu => jeu.nomjeux == nom && jeu.idmj == mj.idperso);
+                if (jeux != null) {
+                    if(newnom != null)  jeux.nomjeux = newnom;
+                    if(newmj != null) jeux.idmj = newmj.idperso;
+                    bdd.SaveChanges();
+                    return true;
+                } else { Console.WriteLine("Le Jeu n'existe pas, impossible de le modifier."); return false; }
+            } catch (Exception e) { Console.WriteLine(e.Message); return false; }
+        }
+        public bool DelJeu(string nom, Personne mj)
+        {
+            try
+            {
+                Jeux jeux = bdd.Jeux.FirstOrDefault(jeu => jeu.nomjeux == nom && jeu.idmj == mj.idperso);
+                if (jeux != null) {
+                    bdd.Jeux.Remove(jeux);
+                    bdd.SaveChanges();
+                    return true;
+                } else { Console.WriteLine("Le Jeu n'existe pas, impossible de la supprimer."); return false; }
+            } catch (Exception e) { Console.WriteLine(e.Message); return false; }
+        }
+        public Jeux GetJeu(string nom, Personne mj) => bdd.Jeux.FirstOrDefault(jeu => jeu.nomjeux == nom && jeu.idmj == mj.idperso);
+        public List<Jeux> GetAllJeuxMJ(Personne mj) => bdd.Jeux.ToList().FindAll(jeu => jeu.idmj == mj.idperso);
+        public List<Jeux> GetAllJeux() => bdd.Jeux.ToList();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BotDiscord/Dal/DalPersonne.cs
@@ -0,0 +1,50 @@
+﻿using BotDiscord.BdD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BotDiscord.Dal
+{
+    class DalPersonne : IDisposable
+    {
+        private ModelRoliste bdd;
+ 
+        public DalPersonne()
+        {
+            bdd = new ModelRoliste();
+        }
+
+        public bool AddPerso(ulong id)
+        {
+            try
+            {
+                Personne perso = bdd.Personne.FirstOrDefault(pe => pe.idperso == (long)id);
+                if (perso == null) {
+                    bdd.Personne.Add(new Personne { idperso = (long)id });
+                    bdd.SaveChanges();
+                    return true;
+                } else { Console.WriteLine("La Personne existe déjà, impossible de la rajouter."); return false; }
+            } catch (Exception e) { Console.WriteLine(e.Message); return false; }
+        }
+        public bool DelPerso(ulong id)
+        {
+            try
+            {
+                Personne perso = bdd.Personne.FirstOrDefault(pe => pe.idperso == (long)id);
+                if (perso != null) {
+                    bdd.Personne.Remove(perso);
+                    bdd.SaveChanges();
+                    return true;
+                } else { Console.WriteLine("La Personne n'existe pas, impossible de la supprimer."); return false; }
+            }
+            catch (Exception e) { Console.WriteLine(e.Message); return false; }
+        }
+        public Personne GetPerso(ulong id) => bdd.Personne.FirstOrDefault(pe => pe.idperso == (long)id);
+        public List<Personne> GetAllPerso() => bdd.Personne.ToList();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException(); 
+        }
+    }
+}
